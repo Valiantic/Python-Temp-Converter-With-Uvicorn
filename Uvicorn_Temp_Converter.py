64a0d892da75a65b562ca0a9bb28d113206da22f
Steven Gabriel Madali <88831228+Valiantic@users.noreply.github.com>
2024-06-03 21:42:58 +0800

    Add files via upload

---
+++ Uvicorn_Temp_Converter.py
@@ -0,0 +1,35 @@
+
+import uvicorn
+from fastapi import FastAPI, Response
+
+app = FastAPI()
+
+
+@app.get("/")
+async def index():
+    ret = '''<html>
+<body>
+    <h1> Temperature Converter Prototype Using Uvicorn </h1>
+    <h4> By Steven Madali </h4>
+</body>
+</html>'''
+    return Response(content=ret, media_type="text/html")
+
+@app.get("/{n1}/{conv}")
+async def docompute(n1:float, conv:str):
+    result = 0
+    if conv == 'c':
+        result = n1 * 9 / 5 + 32
+        message = f"The converted Celsius temperature is equal to {result}°F. Fahrenheit"
+        
+    elif conv == 'f':
+        result = (n1 - 32) * 5 / 9
+        message = f"The converted Fahrenheit temperature is equal to {result}°C. Celsius"
+    elif conv == 'k':
+        result = n1 + 273.5
+        message = f"The converted Celsius temperature is equal to {result}°K. Kelvin"
+        
+    return {"message": message}
+
+if __name__ == "__main__":
+    uvicorn.run("Uvicorn_Temp_Converter:app", host="127.0.0.1", port=5000, reload=True)
